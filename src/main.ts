48d344f535ec31cc72e2aa8a7c617ee69a96c34f
Felipe <felipealencar.0014@gmail.com>
2023-12-07 18:38:26 -0300

    estilização da tela de Login

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import router from './router'
 
 const app = createApp(App)
 
+import './assets/styles/global.scss';
+
 app.use(createPinia())
 app.use(router)
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,9 @@
                 login: '',
                 senha: '',
                 loading: false,
-                erro: ''
+                erro: '',
+                inputLoginFocus: false,
+                inputSenhaFocus: false,
             }
         },
         methods: {
@@ -17,29 +19,44 @@
                 }
 
                 alert('Login efetuado' + this.login + ', ' + this.senha);
+            },
+            setFocusLogin(v :boolean) {
+                this.inputLoginFocus = v;
+            },
+
+            setFocusSenha(v :boolean) {
+                this.inputSenhaFocus = v;
             }
         }
     });
 </script>
 
 <template>
-    <div>
-        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" />
+    <div class="container-publico">
+        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo" />
         <form>
-            <p v-if="erro">{{ erro }}</p>
-            <div>
+            <p v-if="erro" class="erro">{{ erro }}</p>
+            <div class="input" :class="{focus : inputLoginFocus}">
                 <img src="../assets/imagens/envelope.svg" alt="Insira o login" />
-                <input type="text" v-model="login" />
+                <input type="text" v-model="login" placeholder="E-mail"
+                    @focus="setFocusLogin(true)"
+                    @blur="setFocusLogin(false)"
+                />
             </div>
-            <div>
+            <div class="input" :class="{focus : inputSenhaFocus}">
                 <img src="../assets/imagens/chave.svg" alt="Informe a senha" />
-                <input type="password" v-model="senha" />
+                <input type="password" v-model="senha" placeholder="Senha"
+                    @focus="setFocusSenha(true)"
+                    @blur="setFocusSenha(false)"
+                />
             </div>
             <button @click.enter.prevent="efetuarLogin">Login</button>
-            <div>
+            <div class="link">
                 <p>Não possuí uma conta?</p>
                 <a>Faça seu cadastro agora!</a>
             </div>
         </form>
     </div>
 </template>
+
+<style lang="scss" src="@/assets/styles/paginaPublica.scss" />
